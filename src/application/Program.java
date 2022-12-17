95060ae60fb952434e6bf780b79f1bd4d61dbee8
Rafael Ribeiro <rafaelsribeiro45@gmail.com>
2022-12-16 21:09:21 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,6 +36,16 @@ public class Program {
 		Seller sel2 = new Seller(null, "Clara", "clara@gmail.com", new Date(), 2500.00, new Department(3, null));
 		sellerDao.insert(sel2);
 		System.out.println("Inserted! new id = " + sel2.getId());
+
+		System.out.println();
+		
+		System.out.println("==== TEST 5: seller update ====");
+		sel1 = sellerDao.findById(1);
+		sel1.setName("Martha Wayne");
+		sellerDao.update(sel1);
+		System.out.println("Update completed!");
+		
+	
 	}
 	
 }
